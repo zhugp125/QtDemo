1f4ef97c60ab36c31a5dbbbbc937867a94234874
zhugp <18811475054@163.com>
2019-04-27 21:43:22 +0800

    Qt无边框窗体，窗体拖动与放大缩小

---
+++ FramlessWidget/widget.cpp
@@ -0,0 +1,148 @@
+#include "widget.h"
+#include <QPushButton>
+#include <QMouseEvent>
+#include <QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    p_close_button_ = new QPushButton("close", this);
+    p_close_button_->move(20, 20);
+    connect(p_close_button_, &QAbstractButton::clicked, this, &QWidget::close);
+
+    resize(400, 300);
+    setMinimumSize(320, 240);
+    setMaximumSize(600, 500);
+    setMouseTracking(true);
+    setWindowFlags(Qt::FramelessWindowHint | Qt::Widget);
+}
+
+Widget::~Widget()
+{
+}
+
+void Widget::mousePressEvent(QMouseEvent *event)
+{
+    if (event->buttons() & Qt::LeftButton)
+    {
+        start_pos_ = event->pos();
+    }
+    QWidget::mousePressEvent(event);
+}
+
+void Widget::mouseMoveEvent(QMouseEvent *event)
+{
+    if (event->buttons() & Qt::LeftButton)
+    {
+        if (enum_direction_ == NoDir) // 不拖动，只移动窗体位置
+        {
+            move(event->pos() - start_pos_ + pos());
+        }
+        else
+        {
+            int n_left = geometry().left();
+            int n_top = geometry().top();
+            int n_right = geometry().right();
+            int n_bottom = geometry().bottom();
+            // 根据拖动方向，重新计算窗体位置
+            if (enum_direction_ & Top)
+            {
+                n_top = event->globalY();
+            }
+            else if (enum_direction_ & Bottom)
+            {
+                n_bottom = event->globalY();
+            }
+
+            if (enum_direction_ & Left)
+            {
+                n_left = event->globalX();
+            }
+            else if (enum_direction_ & Right)
+            {
+                n_right = event->globalX();
+            }
+
+            // 重新计算后的宽度在最小宽度和最大宽度之外，则计算无效，恢复原先的坐标
+            if (n_right - n_left < minimumWidth() ||
+                n_right - n_left > maximumWidth())
+            {
+                n_right = geometry().right();
+                n_left = geometry().left();
+            }
+            // 重新计算后的高度在最小高度和最大高度之外，则计算无效，恢复原先的坐标
+            if (n_bottom - n_top < minimumHeight() ||
+                n_bottom - n_top > maximumHeight())
+            {
+                n_top = geometry().top();
+                n_bottom = geometry().bottom();
+            }
+            setGeometry(QRect(QPoint(n_left, n_top), QPoint(n_right, n_bottom)));
+        }
+    }
+    else
+    {
+        // 检测边缘
+        testEdge(event->pos());
+    }
+    QWidget::mouseMoveEvent(event);
+}
+
+/*!
+ * \brief setCursorShape 设置鼠标显示形状
+ * \param enum_direction 拖动方向
+ */
+void Widget::setCursorShape(MoveDirection enum_direction)
+{
+    Qt::CursorShape shape = Qt::ArrowCursor;
+    switch(enum_direction)
+    {
+    case TopLeft:
+    case BottomRight:
+        shape = Qt::SizeFDiagCursor;
+        break;
+    case TopRight:
+    case BottomLeft:
+        shape = Qt::SizeBDiagCursor;
+        break;
+    case Top:
+    case Bottom:
+        shape = Qt::SizeVerCursor;
+        break;
+    case Left:
+    case Right:
+        shape = Qt::SizeHorCursor;
+        break;
+    default:
+        break;
+    }
+    setCursor(shape);
+}
+
+/*!
+ * \brief testEdge 窗体边缘检测
+ * \param p        当前坐标位置，相对坐标
+ */
+void Widget::testEdge(const QPoint &p)
+{
+    int enum_direction = NoDir;
+    if (p.x() < num_edge_)
+    {
+        enum_direction |= Left;
+    }
+    else if (p.x() > (width() - num_edge_))
+    {
+        enum_direction |= Right;
+    }
+
+    if (p.y() < num_edge_)
+    {
+        enum_direction |= Top;
+    }
+    else if (p.y() > (height() - num_edge_))
+    {
+        enum_direction |= Bottom;
+    }
+    enum_direction_ = static_cast<MoveDirection>(enum_direction);
+    setCursorShape(enum_direction_);
+}
---
+++ FramlessWidget/widget.h
@@ -0,0 +1,53 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+class QPushButton;
+
+// 鼠标拖动方向
+enum MoveDirection
+{
+    NoDir = 0x00,                       // 无拖动
+    Left = 0x01,                        // 向左拖动
+    Right = 0x02,                       // 向右拖动
+    Top = 0x04,                         // 向上拖动
+    Bottom = 0x08,                      // 向下拖动
+    TopLeft = Top | Left,               // 向左上拖动
+    TopRight = Top | Right,             // 向右上拖动
+    BottomLeft = Bottom | Left,         // 向左下拖动
+    BottomRight = Bottom | Right,       // 向右下拖动
+};
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = 0);
+    ~Widget();
+
+protected:
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+
+private:
+    /*!
+     * \brief setCursorShape 设置鼠标显示形状
+     * \param enum_direction 拖动方向
+     */
+    void setCursorShape(MoveDirection enum_direction);
+    /*!
+     * \brief testEdge 窗体边缘检测
+     * \param p        当前坐标位置，相对坐标
+     */
+    void testEdge(const QPoint &p);
+
+private:
+    QPushButton* p_close_button_ = nullptr; // 关闭按钮
+    QPoint start_pos_;                      // 鼠标落下点的坐标
+    const int num_edge_ = 4;                // 边缘距离
+    MoveDirection enum_direction_ = NoDir;  // 拖动方向
+};
+
+#endif // WIDGET_H
